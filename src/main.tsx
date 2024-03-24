bd274a8d42a2ab869a2ba13af10a5023dbf68722
trunglvb <levubaotrung0311@gmail.com>
2024-03-24 22:35:24 +0700

    [JIRA_CLONE-2]: setting i18n

--- src/main.tsx
+++ src/main.tsx
@@ -5,6 +5,7 @@ import './global.css'
 import { BrowserRouter } from 'react-router-dom'
 import { Provider } from 'react-redux'
 import { store } from './redux/store.ts'
+import '@/i18n/i18n.ts'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
